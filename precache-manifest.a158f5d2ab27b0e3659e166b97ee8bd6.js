cc6a60cb70e0be536b3f76eb156b4301e8eebb40
Usmanchd <usman.aslam0701@gmail.com>
2020-07-28 14:27:17 +0500

    Updates

---
+++ precache-manifest.a158f5d2ab27b0e3659e166b97ee8bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf58206ab73ad301aec02f79806cab14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "35f87efc7b64ecb77719",
+    "url": "/portfolio/static/css/main.b2a91e8a.chunk.css"
+  },
+  {
+    "revision": "62cc5c2ec1b58cce9678",
+    "url": "/portfolio/static/js/2.9f8a5aa6.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/portfolio/static/js/2.9f8a5aa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35f87efc7b64ecb77719",
+    "url": "/portfolio/static/js/main.34687327.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
